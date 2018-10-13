8dd7b9a548c5659e449d798523b05cf4333d99da
github username <github email address>
2018-10-13 20:52:29 +0100

    added create to requests controller and routes

--- app/controllers/api/v1/requests_controller.rb
+++ app/controllers/api/v1/requests_controller.rb
@@ -5,6 +5,15 @@ class Api::V1::RequestsController < ApplicationController
     render json: @requests
   end
 
+  def create
+    @request = Request.new(request_params)
+      if @request.save
+        render json: @request
+      else
+        render json @request.errors
+      end
+  end
+
   def show
     @request = Request.find(params[:id])
     render json: @request
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:index, :show]
       resources :stylist_listings, only: [:index, :show]
-      resources :requests, only: [:index, :show]
+      resources :requests, only: [:index, :show, :new, :create]
       resources :availabilities, only: [:index, :show]
       resources :services, only: [:index]
       resources :bookings, only: [:index, :show]
